334826464f766ff50a4838cd6e56e2ffd05b77bb
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-22 09:44:50 -0500

    created the category create route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,10 +25,35 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price','stock','category_id']
+    }
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'This category does not exist' });
+      return;
+    }
+    res.json(dbCategoryData);
+  }).catch (err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then (dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
