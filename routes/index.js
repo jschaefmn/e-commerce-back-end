c9ca8e02188ad5792e08b9bc02aef4d44e8989c2
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-22 09:24:08 -0500

    added routes to api/index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const categoryRoutes = require('./api/category-routes');
+const productRoutes = require('./api/product-routes');
+const tagRoutes = require('./api/tag-routes');
 
 router.use('/api', apiRoutes);
+router.use('/categories', categoryRoutes);
+route.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
